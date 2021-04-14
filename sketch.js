17ddfacccc0840a1ded26542d4a7b6a6b61830d8
bperez613 <73673462+bperez613@users.noreply.github.com>
2021-04-14 14:52:29 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  <meta charset="UTF-8">
+  <script language="javascript" type="text/javascript" src="https://cdn.jsdelivr.net/npm/p5@1.3.1/lib/p5.min.js"></script>
+  
+  <script language="javascript" src="https://cdn.jsdelivr.net/npm/p5@1.3.1/lib/addons/p5.sound.min.js"></script>
+  <script language="javascript" type="text/javascript" src="sketch.js"></script>
+</head>
+body {
+  background-color: pink;
+}
+</style>
+</head>
+<body>
+
+<h1 style="color:red;">this is our sound page.</h1>
+
